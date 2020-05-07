994b1791bf3f245555ebf0351cdcd87aba6ed6f3
Diep Thuy <boychaos9x@gmail.com>
2020-05-07 04:09:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 
+}
+function nestedTarget(){
+return document.querySelector(' #nested .target')
+}
+
+
+function increaseRankBy(n){
+  const rankList=document
+  .getElementById('app')
+  .querySelectorAll('.ranked-list');
+
+  for (let i=0; i < rankList.length; i++){
+    //since ranked-lists have a bunch of children elements in its
+    let liChildren= rankList[i].children;
+    for (let j =0; j< liChildren.length; j++){
+      liChildren[j].innerHTML= parseInt(liChildren[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild(){
+  let node= document
+  .getElementById('grand-node')
+
+
+  let nextNode = node.children[0]
+  while (nextNode){
+    node= nextNode;
+    nextNode= node.children[0]
+  }
+  return node;
+
+}
